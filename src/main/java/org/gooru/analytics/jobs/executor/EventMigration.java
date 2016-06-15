205c32d7cda6d91c0296bff5e787077c0494c316
Daniel <arokiadaniel.a@bambeeq.com>
2016-06-15 12:25:27 +0530

    formatting and search migration builder format change

--- src/main/java/org/gooru/analytics/jobs/executor/EventMigration.java
+++ src/main/java/org/gooru/analytics/jobs/executor/EventMigration.java
@@ -19,95 +19,87 @@ import com.netflix.astyanax.retry.ConstantBackoff;
 
 import io.vertx.core.json.JsonObject;
 
-public class EventMigration implements JobInitializer{
-	private static final Logger LOG = LoggerFactory.getLogger(EventMigration.class);
-    private static final SimpleDateFormat minuteDateFormatter = new SimpleDateFormat("yyyyMMddkkmm");
-    private static final EventCassandraClusterClient eventCassandraClusterClient = EventCassandraClusterClient
-			.instance();
-    private static final ArchivedCassandraClusterClient archivedCassandraClusterClient = ArchivedCassandraClusterClient
-			.instance();
-    private static final PreparedStatement insertEvents = (eventCassandraClusterClient.getCassandraSession())
-         			.prepare("INSERT INTO events(event_id,fields)VALUES(?,?)");
-    private static final PreparedStatement insertEventTimeLine = (eventCassandraClusterClient.getCassandraSession())
-         			.prepare("INSERT INTO events_timeline(event_time,event_id)VALUES(?,?);");
+public class EventMigration implements JobInitializer {
+  private static final Logger LOG = LoggerFactory.getLogger(EventMigration.class);
+  private static final SimpleDateFormat minuteDateFormatter = new SimpleDateFormat("yyyyMMddkkmm");
+  private static final EventCassandraClusterClient eventCassandraClusterClient = EventCassandraClusterClient.instance();
+  private static final ArchivedCassandraClusterClient archivedCassandraClusterClient = ArchivedCassandraClusterClient.instance();
+  private static final PreparedStatement insertEvents =
+          (eventCassandraClusterClient.getCassandraSession()).prepare("INSERT INTO events(event_id,fields)VALUES(?,?)");
+  private static final PreparedStatement insertEventTimeLine =
+          (eventCassandraClusterClient.getCassandraSession()).prepare("INSERT INTO events_timeline(event_time,event_id)VALUES(?,?);");
 
-    private static class EventMigrationHolder {
-		public static final EventMigration INSTANCE = new EventMigration();
-	}
+  private static class EventMigrationHolder {
+    public static final EventMigration INSTANCE = new EventMigration();
+  }
 
-	public static EventMigration instance() {
-		return EventMigrationHolder.INSTANCE;
-	}
-	
-    public void deployJob(JsonObject config){
+  public static EventMigration instance() {
+    return EventMigrationHolder.INSTANCE;
+  }
 
-		LOG.info("deploying EventMigration....");
-		try {
-			String start = "201508251405";
-			String end = "201508251405";
+  public void deployJob(JsonObject config) {
 
-			Long startTime = minuteDateFormatter.parse(start).getTime();
-			LOG.info("startTime : " + start);
-			Long endTime = minuteDateFormatter.parse(end).getTime();
-			LOG.info("endTime : " + end);
-			// String start = "201508251405";
-			// Long endTime = new Date().getTime();
+    LOG.info("deploying EventMigration....");
+    try {
+      String start = "201508251405";
+      String end = "201508251405";
 
-			for (Long startDate = startTime; startDate < endTime;) {
-				String currentDate = minuteDateFormatter.format(new Date(startDate));
-				LOG.info("Running for :" + currentDate);
-				// Incrementing time - one minute
-				ColumnList<String> et = readWithKey(Constants.EVENT_TIMIELINE, currentDate);
-				for (String eventId : et.getColumnNames()) {
-					ColumnList<String> ef = readWithKey(Constants.EVENT_DETAIL,
-							et.getStringValue(eventId, Constants.NA));
-					// Insert event_time_line
-					insertData(currentDate, et.getStringValue(eventId, Constants.NA), insertEventTimeLine);
-					// Insert events
-					insertData(et.getStringValue(eventId, Constants.NA),
-							ef.getStringValue(Constants.FIELDS, Constants.NA), insertEvents);
-				}
-				startDate = new Date(startDate).getTime() + 60000;
-				Thread.sleep(200);
-			}
-        } catch (ParseException | InterruptedException e) {
-			if (e instanceof ArrayIndexOutOfBoundsException) {
-				LOG.info("startTime or endTime can not be null. Please make sure the class execution format as below.");
-				LOG.info(
-						"java -classpath build/libs/migration-scripts-fat.jar: org.gooru.migration.jobs.EventMigration 201508251405 201508251410");
-			} else {
-                LOG.error("Something went wrong...", e);
-			}
-			System.exit(500);
-		}
-	   	
+      Long startTime = minuteDateFormatter.parse(start).getTime();
+      LOG.info("startTime : " + start);
+      Long endTime = minuteDateFormatter.parse(end).getTime();
+      LOG.info("endTime : " + end);
+      // String start = "201508251405";
+      // Long endTime = new Date().getTime();
+
+      for (Long startDate = startTime; startDate < endTime;) {
+        String currentDate = minuteDateFormatter.format(new Date(startDate));
+        LOG.info("Running for :" + currentDate);
+        // Incrementing time - one minute
+        ColumnList<String> et = readWithKey(Constants.EVENT_TIMIELINE, currentDate);
+        for (String eventId : et.getColumnNames()) {
+          ColumnList<String> ef = readWithKey(Constants.EVENT_DETAIL, et.getStringValue(eventId, Constants.NA));
+          // Insert event_time_line
+          insertData(currentDate, et.getStringValue(eventId, Constants.NA), insertEventTimeLine);
+          // Insert events
+          insertData(et.getStringValue(eventId, Constants.NA), ef.getStringValue(Constants.FIELDS, Constants.NA), insertEvents);
+        }
+        startDate = new Date(startDate).getTime() + 60000;
+        Thread.sleep(200);
+      }
+    } catch (ParseException | InterruptedException e) {
+      if (e instanceof ArrayIndexOutOfBoundsException) {
+        LOG.info("startTime or endTime can not be null. Please make sure the class execution format as below.");
+        LOG.info("java -classpath build/libs/migration-scripts-fat.jar: org.gooru.migration.jobs.EventMigration 201508251405 201508251410");
+      } else {
+        LOG.error("Something went wrong...", e);
+      }
+      System.exit(500);
     }
 
-	public static ColumnList<String> readWithKey(String cfName, String key) {
+  }
 
-		ColumnList<String> result = null;
-		try {
-			result = (archivedCassandraClusterClient.getCassandraKeyspace())
-					.prepareQuery(archivedCassandraClusterClient.accessColumnFamily(cfName))
-					.setConsistencyLevel(ConsistencyLevel.CL_QUORUM).withRetryPolicy(new ConstantBackoff(2000, 5))
-					.getKey(key).execute().getResult();
+  public static ColumnList<String> readWithKey(String cfName, String key) {
 
-        } catch (Exception e) {
-            LOG.error("Failure in reading with key", e);
-        }
+    ColumnList<String> result = null;
+    try {
+      result = (archivedCassandraClusterClient.getCassandraKeyspace()).prepareQuery(archivedCassandraClusterClient.accessColumnFamily(cfName))
+              .setConsistencyLevel(ConsistencyLevel.CL_QUORUM).withRetryPolicy(new ConstantBackoff(2000, 5)).getKey(key).execute().getResult();
 
-		return result;
-	}
+    } catch (Exception e) {
+      LOG.error("Failure in reading with key", e);
+    }
 
-	public static void insertData(String key, String column, PreparedStatement preparedStatement) {
-		try {
-			BoundStatement boundStatement = new BoundStatement(preparedStatement);
-			boundStatement.bind(key, column);
-			(eventCassandraClusterClient.getCassandraSession()).executeAsync(boundStatement);
-        } catch (Exception e) {
-            LOG.error("Inserting Data failed with exception: ", e);
-		}
-	}
+    return result;
+  }
 
+  public static void insertData(String key, String column, PreparedStatement preparedStatement) {
+    try {
+      BoundStatement boundStatement = new BoundStatement(preparedStatement);
+      boundStatement.bind(key, column);
+      (eventCassandraClusterClient.getCassandraSession()).executeAsync(boundStatement);
+    } catch (Exception e) {
+      LOG.error("Inserting Data failed with exception: ", e);
+    }
+  }
 
 }
--- src/main/java/org/gooru/analytics/jobs/executor/SyncContentAuthorizedUsers.java
+++ src/main/java/org/gooru/analytics/jobs/executor/SyncContentAuthorizedUsers.java
@@ -28,111 +28,107 @@ import com.datastax.driver.core.querybuilder.QueryBuilder;
 import io.vertx.core.json.JsonObject;
 
 public class SyncContentAuthorizedUsers implements JobInitializer {
-	private static final Timer timer = new Timer();
-	private static final String JOB_NAME = "sync_content_authorized_users";
-	private static final AnalyticsUsageCassandraClusterClient analyticsUsageCassandraClusterClient = AnalyticsUsageCassandraClusterClient
-			.instance();
-	private static final PostgreSQLConnection postgreSQLConnection = PostgreSQLConnection.instance();
-	private static final Logger LOG = LoggerFactory.getLogger(SyncContentAuthorizedUsers.class);
-	private static final String GET_AUTHORIZED_USERS_QUERY = "select id,creator_id,collaborator,updated_at from class where class.updated_at > to_timestamp(?,'YYYY-MM-DD HH24:MI:SS') - interval '3 minutes';";
-	private static String currentTime = null;
-    private static final SimpleDateFormat minuteDateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    private static long JOB_INTERVAL = 6000L;
-    
-    private static class SyncContentAuthorizedUsersHolder {
-		public static final SyncContentAuthorizedUsers INSTANCE = new SyncContentAuthorizedUsers();
-	}
+  private static final Timer timer = new Timer();
+  private static final String JOB_NAME = "sync_content_authorized_users";
+  private static final AnalyticsUsageCassandraClusterClient analyticsUsageCassandraClusterClient = AnalyticsUsageCassandraClusterClient.instance();
+  private static final PostgreSQLConnection postgreSQLConnection = PostgreSQLConnection.instance();
+  private static final Logger LOG = LoggerFactory.getLogger(SyncContentAuthorizedUsers.class);
+  private static final String GET_AUTHORIZED_USERS_QUERY =
+          "select id,creator_id,collaborator,updated_at from class where class.updated_at > to_timestamp(?,'YYYY-MM-DD HH24:MI:SS') - interval '3 minutes';";
+  private static String currentTime = null;
+  private static final SimpleDateFormat minuteDateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  private static long JOB_INTERVAL = 6000L;
 
-	public static SyncContentAuthorizedUsers instance() {
-		return SyncContentAuthorizedUsersHolder.INSTANCE;
-	}
-	
-    public void deployJob(JsonObject config){
-    	LOG.info("deploying SyncContentAuthorizedUsers....");
-    	JOB_INTERVAL = config.getLong("content.authorizers.sync.delay");
-		minuteDateFormatter.setTimeZone(TimeZone.getTimeZone(Constants.UTC));
-		final String jobLastUpdatedTime = getLastUpdatedTime();
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				postgreSQLConnection.initializeComponent(config);
-				if (currentTime != null) {
-					currentTime = minuteDateFormatter.format(new Date());
-				} else {
-					currentTime = jobLastUpdatedTime;
-				}
-				LOG.info("currentTime:" + currentTime);
-				String updatedTime = null;
-				List<Map> classList = Base.findAll(GET_AUTHORIZED_USERS_QUERY, currentTime);
-				for (Map classCourseDetail : classList) {
-					String classId = classCourseDetail.get(Constants.ID).toString();
-					String creator = classCourseDetail.get(Constants.CREATOR_ID).toString();
-					Object collabs = classCourseDetail.get(Constants.COLLABORATOR);
-					updatedTime = classCourseDetail.get(Constants.UPDATED_AT).toString();
-					HashSet<String> collaborators = null;
-					LOG.info("class : " + classId);
-					if (collabs != null) {
-						JSONArray collabsArray = new JSONArray(collabs.toString());
-						collaborators = new HashSet<>();
-						for (int index = 0; index < collabsArray.length(); index++) {
-							collaborators.add(collabsArray.getString(index));
-						}
-					}
-					updateAuthorizedUsers(classId, creator, collaborators);
-				}
-				updateLastUpdatedTime(JOB_NAME, updatedTime == null ? currentTime : updatedTime);
-				postgreSQLConnection.finalizeComponent();
-			}
-		};
-		timer.scheduleAtFixedRate(task, 0, JOB_INTERVAL);
-	
+  private static class SyncContentAuthorizedUsersHolder {
+    public static final SyncContentAuthorizedUsers INSTANCE = new SyncContentAuthorizedUsers();
+  }
+
+  public static SyncContentAuthorizedUsers instance() {
+    return SyncContentAuthorizedUsersHolder.INSTANCE;
+  }
+
+  public void deployJob(JsonObject config) {
+    LOG.info("deploying SyncContentAuthorizedUsers....");
+    JOB_INTERVAL = config.getLong("content.authorizers.sync.delay");
+    minuteDateFormatter.setTimeZone(TimeZone.getTimeZone(Constants.UTC));
+    final String jobLastUpdatedTime = getLastUpdatedTime();
+    TimerTask task = new TimerTask() {
+      @Override
+      public void run() {
+        postgreSQLConnection.initializeComponent(config);
+        if (currentTime != null) {
+          currentTime = minuteDateFormatter.format(new Date());
+        } else {
+          currentTime = jobLastUpdatedTime;
+        }
+        LOG.info("currentTime:" + currentTime);
+        String updatedTime = null;
+        List<Map> classList = Base.findAll(GET_AUTHORIZED_USERS_QUERY, currentTime);
+        for (Map classCourseDetail : classList) {
+          String classId = classCourseDetail.get(Constants.ID).toString();
+          String creator = classCourseDetail.get(Constants.CREATOR_ID).toString();
+          Object collabs = classCourseDetail.get(Constants.COLLABORATOR);
+          updatedTime = classCourseDetail.get(Constants.UPDATED_AT).toString();
+          HashSet<String> collaborators = null;
+          LOG.info("class : " + classId);
+          if (collabs != null) {
+            JSONArray collabsArray = new JSONArray(collabs.toString());
+            collaborators = new HashSet<>();
+            for (int index = 0; index < collabsArray.length(); index++) {
+              collaborators.add(collabsArray.getString(index));
+            }
+          }
+          updateAuthorizedUsers(classId, creator, collaborators);
+        }
+        updateLastUpdatedTime(JOB_NAME, updatedTime == null ? currentTime : updatedTime);
+        postgreSQLConnection.finalizeComponent();
+      }
+    };
+    timer.scheduleAtFixedRate(task, 0, JOB_INTERVAL);
+
+  }
+
+  public SyncContentAuthorizedUsers() {
+  }
+
+  private static void updateAuthorizedUsers(String classId, String creator, HashSet<String> collabs) {
+    try {
+      Insert insert = QueryBuilder.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.CONTENT_AUTHORIZED_USERS)
+              .value(Constants._GOORU_OID, classId).value(Constants.COLLABORATORS, collabs).value(Constants.CREATOR_UID, creator);
+
+      ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(insert);
+      resultSetFuture.get();
+    } catch (Exception e) {
+      LOG.error("Error while updating authorized user details.", e);
     }
-	public SyncContentAuthorizedUsers()  {}
+  }
 
-	private static void updateAuthorizedUsers(String classId, String creator, HashSet<String> collabs) {
-		try {
-			Insert insert = QueryBuilder
-					.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(),
-							Constants.CONTENT_AUTHORIZED_USERS)
-					.value(Constants._GOORU_OID, classId).value(Constants.COLLABORATORS, collabs).value(Constants.CREATOR_UID, creator);
+  private static String getLastUpdatedTime() {
+    try {
+      Statement select = QueryBuilder.select().all()
+              .from(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
+              .where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME));
+      ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(select);
+      ResultSet result = resultSetFuture.get();
+      for (Row r : result) {
+        return r.getString(Constants.PROPERTY_VALUE);
+      }
+    } catch (Exception e) {
+      LOG.error("Error while reading job last updated time.{}", e);
+    }
+    return minuteDateFormatter.format(new Date());
+  }
 
-			ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession()
-					.executeAsync(insert);
-			resultSetFuture.get();
-		} catch (Exception e) {
-			LOG.error("Error while updating authorized user details.", e);
-		}
-	}
+  private static void updateLastUpdatedTime(String jobName, String updatedTime) {
+    try {
+      Insert insertStatmt = QueryBuilder.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
+              .value(Constants._JOB_NAME, jobName).value(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME)
+              .value(Constants.PROPERTY_VALUE, updatedTime);
 
-	private static String getLastUpdatedTime() {
-		try {
-			Statement select = QueryBuilder.select().all()
-					.from(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
-					.where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME));
-			ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession()
-					.executeAsync(select);
-			ResultSet result = resultSetFuture.get();
-			for (Row r : result) {
-				return r.getString(Constants.PROPERTY_VALUE);
-			}
-		} catch (Exception e) {
-			LOG.error("Error while reading job last updated time.{}", e);
-		}
-		return minuteDateFormatter.format(new Date());
-	}
-
-	private static void updateLastUpdatedTime(String jobName, String updatedTime) {
-		try {
-			Insert insertStatmt = QueryBuilder
-					.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
-					.value(Constants._JOB_NAME, jobName).value(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME)
-					.value(Constants.PROPERTY_VALUE, updatedTime);
-
-			ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession()
-					.executeAsync(insertStatmt);
-			resultSetFuture.get();
-		} catch (Exception e) {
-			LOG.error("Error while updating last updated time.", e);
-		}
-	}
+      ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(insertStatmt);
+      resultSetFuture.get();
+    } catch (Exception e) {
+      LOG.error("Error while updating last updated time.", e);
+    }
+  }
 }
