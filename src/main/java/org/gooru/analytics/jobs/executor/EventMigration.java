8e6de6a27990457292d98c234dd65cfbf88c0a58
daniel <arokiadaniel.a@bambeeq.com>
2016-07-12 14:07:40 +0530

    perf debugging

--- src/main/java/org/gooru/analytics/jobs/executor/EventMigration.java
+++ src/main/java/org/gooru/analytics/jobs/executor/EventMigration.java
@@ -1,6 +1,5 @@
 package org.gooru.analytics.jobs.executor;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,7 +17,6 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.ResultSetFuture;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Statement;
-import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.netflix.astyanax.model.ColumnList;
 import com.netflix.astyanax.model.ConsistencyLevel;
@@ -51,38 +49,41 @@ public class EventMigration implements JobInitializer {
     LOG.info("deploying EventMigration....");
     try {
       String status = getJobStatus();
-      if(!Constants.STOP.equalsIgnoreCase(status)){
-      //String start = "201508251405";
-    //  String end = "201508251405";
-      String start = getLastUpdatedTime();
-      
-      Long startTime = minuteDateFormatter.parse(start).getTime();
-      LOG.info("startTime : " + start);
-      /*Long endTime = minuteDateFormatter.parse(end).getTime();
-      LOG.info("endTime : " + end);*/
-      // String start = "201508251405";
-       Long endTime = new Date().getTime();
-       LOG.info("endTime : " + endTime);
-      for (Long startDate = startTime; startDate < endTime;) {
-        String currentDate = minuteDateFormatter.format(new Date(startDate));
-        LOG.info("Running for :" + currentDate);
-        // Incrementing time - one minute
-        ColumnList<String> et = readWithKey(Constants.EVENT_TIMIELINE, currentDate);
-        for (String eventId : et.getColumnNames()) {
-          ColumnList<String> ef = readWithKey(Constants.EVENT_DETAIL, et.getStringValue(eventId, Constants.NA));
-          // Insert event_time_line
-          insertData(currentDate, et.getStringValue(eventId, Constants.NA), insertEventTimeLine);
-          // Insert events
-          insertData(et.getStringValue(eventId, Constants.NA), ef.getStringValue(Constants.FIELDS, Constants.NA), insertEvents);
+      if (!Constants.STOP.equalsIgnoreCase(status)) {
+        String start = getLastUpdatedTime();
+
+        Long startTime = minuteDateFormatter.parse(start).getTime();
+        LOG.info("startTime : " + start);
+        Long endTime = new Date().getTime();
+        LOG.info("endTime : " + endTime);
+        for (Long startDate = startTime; startDate < endTime;) {
+          String currentDate = minuteDateFormatter.format(new Date(startDate));
+          LOG.info("Running for :" + currentDate);
+          // Incrementing time - one minute
+          long s = System.currentTimeMillis();
+          ColumnList<String> et = readWithKey(Constants.EVENT_TIMIELINE, currentDate);
+          long st = System.currentTimeMillis();
+          LOG.info("time to get data from event_timeline: " + (st-s));
+          for (String eventId : et.getColumnNames()) {
+            LOG.info("eventId: " + eventId);
+            long s1 = System.currentTimeMillis();
+            ColumnList<String> ef = readWithKey(Constants.EVENT_DETAIL, eventId);
+            long s2 = System.currentTimeMillis();
+            LOG.info("time to get data from event_detail: " + (s2-s1));
+            // Insert event_time_line
+            insertData(currentDate, et.getStringValue(eventId, Constants.NA), insertEventTimeLine);
+            // Insert events
+            insertData(eventId, ef.getStringValue(Constants.FIELDS, Constants.NA), insertEvents);
+            long s3 = System.currentTimeMillis();
+            LOG.info("time to write: " + (s3-s2));
+          }
+          startDate = new Date(startDate).getTime() + 60000;
+          updateLastUpdatedTime(JOB_NAME, currentDate);
         }
-        startDate = new Date(startDate).getTime() + 60000;
-        Thread.sleep(200);
-        updateLastUpdatedTime(JOB_NAME, currentDate);
-      }
-      }else{
+      } else {
         LOG.info("Event migration job stopped!!");
       }
-    } catch (ParseException | InterruptedException e) {
+    } catch (Exception e) {
       if (e instanceof ArrayIndexOutOfBoundsException) {
         LOG.info("startTime or endTime can not be null. Please make sure the class execution format as below.");
         LOG.info("java -classpath build/libs/migration-scripts-fat.jar: org.gooru.migration.jobs.EventMigration 201508251405 201508251410");
@@ -99,7 +100,7 @@ public class EventMigration implements JobInitializer {
     ColumnList<String> result = null;
     try {
       result = (archivedCassandraClusterClient.getCassandraKeyspace()).prepareQuery(archivedCassandraClusterClient.accessColumnFamily(cfName))
-              .setConsistencyLevel(ConsistencyLevel.CL_QUORUM).withRetryPolicy(new ConstantBackoff(2000, 5)).getKey(key).execute().getResult();
+              .setConsistencyLevel(ConsistencyLevel.CL_LOCAL_QUORUM).withRetryPolicy(new ConstantBackoff(2000, 5)).getKey(key).execute().getResult();
 
     } catch (Exception e) {
       LOG.error("Failure in reading with key", e);
@@ -122,7 +123,7 @@ public class EventMigration implements JobInitializer {
     try {
       Statement select = QueryBuilder.select().all()
               .from(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
-              .where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME));
+              .where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME)).setConsistencyLevel(com.datastax.driver.core.ConsistencyLevel.QUORUM);
       ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(select);
       ResultSet result = resultSetFuture.get();
       for (Row r : result) {
@@ -133,11 +134,12 @@ public class EventMigration implements JobInitializer {
     }
     return minuteDateFormatter.format(new Date());
   }
+
   private static String getJobStatus() {
     try {
-      Statement select = QueryBuilder.select().all()
-              .from(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
-              .where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.STATUS));
+      Statement select =
+              QueryBuilder.select().all().from(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
+                      .where(QueryBuilder.eq(Constants._JOB_NAME, JOB_NAME)).and(QueryBuilder.eq(Constants.PROPERTY_NAME, Constants.STATUS)).setConsistencyLevel(com.datastax.driver.core.ConsistencyLevel.QUORUM);
       ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(select);
       ResultSet result = resultSetFuture.get();
       for (Row r : result) {
@@ -148,11 +150,12 @@ public class EventMigration implements JobInitializer {
     }
     return Constants.STOP;
   }
+
   private static void updateLastUpdatedTime(String jobName, String updatedTime) {
     try {
-      Insert insertStatmt = QueryBuilder.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
+      Statement insertStatmt = QueryBuilder.insertInto(analyticsUsageCassandraClusterClient.getAnalyticsCassKeyspace(), Constants.SYNC_JOBS_PROPERTIES)
               .value(Constants._JOB_NAME, jobName).value(Constants.PROPERTY_NAME, Constants.LAST_UPDATED_TIME)
-              .value(Constants.PROPERTY_VALUE, updatedTime);
+              .value(Constants.PROPERTY_VALUE, updatedTime).setConsistencyLevel(com.datastax.driver.core.ConsistencyLevel.QUORUM);
 
       ResultSetFuture resultSetFuture = analyticsUsageCassandraClusterClient.getCassandraSession().executeAsync(insertStatmt);
       resultSetFuture.get();
