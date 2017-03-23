a14a710cd7da21c1ec1900cbd8960c68d0cef09b
Daniel <arokiadaniel.a@bambeeq.com>
2017-03-23 16:12:27 +0530

    Migrating 3.0 events

---
+++ nucleus-replay-events/src/main/java/org/gooru/nucleus/replay/jobs/constants/ConfigConstants.java
@@ -0,0 +1,10 @@
+package org.gooru.nucleus.replay.jobs.constants;
+
+public final class ConfigConstants {
+  
+  public static final String EVENTS = "events";
+  
+  public static final String EVENTS_TIMELINE = "events_TIMELINE";
+  
+  public static final String API_END_POINT = "api.end.point";
+}
