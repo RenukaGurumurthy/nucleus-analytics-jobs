b69337da9d766edec6a1dbbba833fd0719881497
Daniel <arokiadaniel.a@bambeeq.com>
2017-06-27 14:40:14 +0530

    Event sync for 4.0

--- nucleus-replay-events/src/main/java/org/gooru/nucleus/replay/jobs/constants/AttributeConstants.java
+++ nucleus-replay-events/src/main/java/org/gooru/nucleus/replay/jobs/constants/AttributeConstants.java
@@ -7,6 +7,19 @@ public class AttributeConstants {
   public static final String EVENT_ID = "event_id";
   
   public static final String FIELDS = "fields";
+  
+  public static final String PROPERTY_NAME = "property_name";
 
+  public static final String PROPERTY_VALUE = "property_value";
+
+  public static final String PROP_START_TIME = "start.time";
+  
+  public static final String PROP_END_TIME = "end.time";
+
+  public static final String PROP_EVENT_REPLAY_JOB_STATUS = "event.replay.status";
+ 
+  public static final String PROP_COMPLETED = "completed";
+
+  public static final String PROP_INPROGRESS = "in-progress";
 
 }
